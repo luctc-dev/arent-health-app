33a86288c095f35ab26de8569464ed66d6be5415
Tran Cong Luc <luctc.developer1902@gmail.com>
2025-02-19 23:49:15 +0700

    init project with nextjs

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+  return (
+    <div className="text-primary-300">Login Page</div>
+  )
+}
+
+export default LoginPage
---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-300': '#FFCC21',
+        'primary-400': '#FF963C',
+        'primary-500': '#EA6C00',
+        'secondary-300': '#8FE9D0',
+        'dark-300': '#2E2E2E',
+        'dark-400': '#777777',
+        'dark-500': '#414141',
+        'light': '#FFFFFF',
+        'black': '#000000',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
